1c07d3c4dd3cf03600da8736c21f6adb33661b22
Ana Mateska <63292673+anamateska@users.noreply.github.com>
2022-04-24 22:50:09 +0200

    Update BookRestController.java

--- src/main/java/mk/ukim/finki/emt/homework2/web/BookRestController.java
+++ src/main/java/mk/ukim/finki/emt/homework2/web/BookRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://git.heroku.com/spring-boot-anamateska.git")
 @RequestMapping("/api/books")
 public class BookRestController {
 
