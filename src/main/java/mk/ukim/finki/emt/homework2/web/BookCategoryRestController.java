604014bdc4a764117365b4576c48bc33199f7cac
Ana Mateska <63292673+anamateska@users.noreply.github.com>
2022-04-24 22:49:56 +0200

    Update BookCategoryRestController.java

--- src/main/java/mk/ukim/finki/emt/homework2/web/BookCategoryRestController.java
+++ src/main/java/mk/ukim/finki/emt/homework2/web/BookCategoryRestController.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://git.heroku.com/spring-boot-anamateska.git")
 @RequestMapping("/api/categories")
 public class BookCategoryRestController {
 
